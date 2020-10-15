8cfd5a2cc0f821011a90ebf8257861edb24c0b2c
Alexander Mentyu <notuxius@gmail.com>
2020-10-16 00:20:02 +0300

    Add more folders features

--- order_media_files.py
+++ order_media_files.py
@@ -2,8 +2,8 @@
 
 import os
 import shutil
-
-curr_folder = os.path.dirname(os.path.realpath(__file__))
+from os.path import expanduser
+from shutil import Error
 
 img_exts = [
     "png",
@@ -36,20 +36,16 @@ vid_exts = [
 ]
 
 
-def list_media(exts):
-    media_files = []
+imgs = []
+vids = []
 
-    for ext in exts:
-        media_files += [file for file in os.listdir(curr_folder) if file.endswith(ext)]
+# curr_folder = os.path.dirname(os.path.realpath(__file__))
+home_folder = expanduser("~")
+docs_folder = os.path.join(home_folder, "Documents")
+chrome_download_folder = os.path.join(docs_folder, "chrome_download")
 
-    return media_files
-
-
-imgs = list_media(img_exts)
-vids = list_media(vid_exts)
-
-imgs_folder = os.path.join(curr_folder, "IMAGES")
-vids_folder = os.path.join(curr_folder, "VIDEOS")
+imgs_folder = os.path.join(docs_folder, "IMAGES")
+vids_folder = os.path.join(docs_folder, "VIDEOS")
 
 
 def create_folder_if_not_exists(folder):
@@ -57,14 +53,38 @@ def create_folder_if_not_exists(folder):
         os.mkdir(folder)
 
 
-def move_files(files, files_folder):
+for dst_folder in (imgs_folder, vids_folder):
+    create_folder_if_not_exists(dst_folder)
+
+
+def list_media(folder, exts):
+    media_files = []
+
+    for ext in exts:
+        media_files += [file for file in os.listdir(folder) if file.endswith(ext)]
+
+    return media_files
+
+
+def move_files(files, src_folder, dst_folder):
     for file in files:
-        print(file)
-        shutil.move(os.path.join(curr_folder, file), files_folder)
+        try:
+            shutil.move(os.path.join(src_folder, file), dst_folder)
+
+        except shutil.Error as e:
+            print(e)
+            # print("Removing the existing file")
+            # os.remove(os.path.join(src_folder, file))
 
 
-for folder in (imgs_folder, vids_folder):
-    create_folder_if_not_exists(folder)
+for src_folder in (docs_folder, chrome_download_folder):
+    try:
+        imgs += list_media(src_folder, img_exts)
+        vids += list_media(src_folder, vid_exts)
 
-move_files(imgs, imgs_folder)
-move_files(vids, vids_folder)
+        move_files(imgs, src_folder, imgs_folder)
+        move_files(vids, src_folder, vids_folder)
+
+    # if no source folder
+    except FileNotFoundError:
+        pass
