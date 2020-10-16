3dea1f4e9e8e3aedae62295886f85348080163de
Alexander Mentyu <notuxius@gmail.com>
2020-10-16 17:55:09 +0300

    Add prompts, found files count

--- media_files_ordinator.py
+++ media_files_ordinator.py
@@ -2,7 +2,10 @@
 
 import os
 import shutil
+import sys
 from os.path import expanduser
+
+import click
 
 img_exts = [
     "png",
@@ -49,13 +52,22 @@ def create_folder_if_not_exists(folder):
         os.mkdir(folder)
 
 
-def list_media_files(folder, exts):
-    media_files = []
+def list_files(folder, exts):
+    files = []
+    num_of_files_ext = {}
 
     for ext in exts:
-        media_files += [file for file in os.listdir(folder) if file.endswith(ext)]
+        # files += [file for file in os.listdir(folder) if file.endswith(ext)]
+        for file in os.listdir(folder):
+            if file.endswith(ext):
+                files.append(file)
+                num_of_files_ext[ext] = num_of_files_ext.get(ext, 0) + 1
 
-    return media_files
+    if files:
+        for ext, num_of_files in num_of_files_ext.items():
+            print(f"Number of {ext.upper()} files: {num_of_files}")
+
+    return files
 
 
 def move_files(files, src_folder, dst_folder):
@@ -63,6 +75,7 @@ def move_files(files, src_folder, dst_folder):
         try:
             shutil.move(os.path.join(src_folder, file), dst_folder)
 
+        # if file already exists or other error
         except shutil.Error as e:
             print(e)
             print((os.path.join(src_folder, file)))
@@ -70,30 +83,37 @@ def move_files(files, src_folder, dst_folder):
             # os.remove(os.path.join(src_folder, file))
 
 
-def order_media_files(*src_folders):
+def order_files(*src_folders):
     for src_folder in src_folders:
         try:
             imgs = []
             vids = []
 
-            imgs += list_media_files(src_folder, img_exts)
-            vids += list_media_files(src_folder, vid_exts)
+            print(f"Locating files in {src_folder} folder:")
 
-            media_files = {imgs_dst_folder: imgs, vids_dst_folder: vids}
+            imgs += list_files(src_folder, img_exts)
+            vids += list_files(src_folder, vid_exts)
 
-            for media_folder in media_files:
-                if media_files[media_folder]:
-                    create_folder_if_not_exists(media_folder)
+            if not imgs and not vids:
+                print("No files to move in the folder")
+                continue
+
+            files = {imgs_dst_folder: imgs, vids_dst_folder: vids}
+
+            if click.confirm(f"Move files from the folder?", default=True):
+                for folder in files:
+                    create_folder_if_not_exists(folder)
 
                     move_files(
-                        media_files[media_folder],
+                        files[folder],
                         src_folder,
-                        media_folder,
+                        folder,
                     )
 
-        # if no source folder
+        # if there is no source folder
         except FileNotFoundError:
             pass
 
 
-order_media_files(docs_src_folder, chrome_download_src_folder)
+if __name__ == "__main__":
+    order_files(docs_src_folder, chrome_download_src_folder)
