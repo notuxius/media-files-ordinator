5331769ba18c276919e0f049450a571c7a2da5df
Alexander Mentyu <notuxius@gmail.com>
2020-10-16 10:00:28 +0300

    Bug fixes, create media folders if necessary

--- order_media_files.py
+++ order_media_files.py
@@ -3,7 +3,6 @@
 import os
 import shutil
 from os.path import expanduser
-from shutil import Error
 
 img_exts = [
     "png",
@@ -35,26 +34,18 @@ vid_exts = [
     "ogg",
 ]
 
-
-imgs = []
-vids = []
-
 # curr_folder = os.path.dirname(os.path.realpath(__file__))
 home_folder = expanduser("~")
 docs_folder = os.path.join(home_folder, "Documents")
 chrome_download_folder = os.path.join(docs_folder, "chrome_download")
 
-imgs_folder = os.path.join(docs_folder, "IMAGES")
-vids_folder = os.path.join(docs_folder, "VIDEOS")
+imgs_folder = os.path.join(docs_folder, "_IMAGES")
+vids_folder = os.path.join(docs_folder, "_VIDEOS")
 
 
 def create_folder_if_not_exists(folder):
     if not os.path.exists(folder):
         os.mkdir(folder)
-
-
-for dst_folder in (imgs_folder, vids_folder):
-    create_folder_if_not_exists(dst_folder)
 
 
 def list_media(folder, exts):
@@ -73,17 +64,30 @@ def move_files(files, src_folder, dst_folder):
 
         except shutil.Error as e:
             print(e)
+            print((os.path.join(src_folder, file)))
             # print("Removing the existing file")
             # os.remove(os.path.join(src_folder, file))
 
 
 for src_folder in (docs_folder, chrome_download_folder):
     try:
+        imgs = []
+        vids = []
+
         imgs += list_media(src_folder, img_exts)
         vids += list_media(src_folder, vid_exts)
 
-        move_files(imgs, src_folder, imgs_folder)
-        move_files(vids, src_folder, vids_folder)
+        media_files = {imgs_folder: imgs, vids_folder: vids}
+
+        for media_folder in media_files:
+            if media_files[media_folder]:
+                create_folder_if_not_exists(media_folder)
+
+                move_files(
+                    media_files[media_folder],
+                    src_folder,
+                    media_folder,
+                )
 
     # if no source folder
     except FileNotFoundError:
